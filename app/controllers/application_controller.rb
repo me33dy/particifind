45e83432f7fb782ff87e9f8ad17eb0e4be923e3a
dinnagonzales <dinnagonzales.05@gmail.com>
2014-09-18 16:15:55 -0700

    home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include ApplicationHelper
+
+  # favicon use on Rails 4
+  config.relative_url_root = "" 
 end
